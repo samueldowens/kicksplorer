3a31e733c799c945712a58cae881ae0781c29c14
samueldowens <samueldowens@gmail.com>
2013-12-18 17:30:20 -0500

    initial commit with project model added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kicksplorer::Application.config.secret_key_base = '5aac2be7a93425c021f9a0b6ad1f7be9c94530f7cc9856b42903ba03a7605dc6a97de47090eb37dbdcbf4e76045de650c829551ad4210dec1123c0fec0fc6128'
---
+++ db/migrate/20131218222538_create_projects.rb
@@ -0,0 +1,24 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :blurb
+      t.integer :goal
+      t.integer :pledged
+      t.string :state
+      t.string :country
+      t.string :currency
+      t.string :currency_symbol
+      t.datetime :deadline
+      t.datetime :created_at
+      t.datetime :launched_at
+      t.integer :backers_count
+      t.string :photo
+      t.string :creator
+      t.string :location
+      t.string :category
+      t.string :urls
+      t.timestamps
+    end
+  end
+end
